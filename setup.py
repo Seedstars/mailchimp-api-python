ea68ef28c2a32b1e2e917897fd4cc23d08848250
Pedro Gomes <pedrorodriguesgomes@gmail.com>
2017-06-19 13:04:40 +0100

    Add new release version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os.path
 
 setup(
     name = 'mailchimp',
-    version = '2.0.9',
+    version = '2.0.9.1',
     author = 'MailChimp Devs',
     author_email = 'api@mailchimp.com',
     description = 'A CLI client and Python API library for the MailChimp email platform.',
